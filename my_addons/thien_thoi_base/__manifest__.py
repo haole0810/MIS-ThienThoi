4ca0dd2286354537da3d7999f5a6310aaaae9f8f
fiz-ivy <kiraranghe@gmail.com>
2026-04-12 16:07:50 +0700

    update code

--- my_addons/thien_thoi_base/__manifest__.py
+++ my_addons/thien_thoi_base/__manifest__.py
@@ -3,7 +3,7 @@
     'summary': 'Danh mục dùng chung: Sản phẩm, Đối tác, Kho hàng',
     'version': '1.0',
     'author': 'Lê Tấn Hào - HCMC University of Transport',
-    'depends': ['base','product', 'stock', 'quan_ly_nhap'], 
+    'depends': ['base','product', 'stock'], 
     'data': [
         'security/ir.model.access.csv',
         'views/base_views.xml'
